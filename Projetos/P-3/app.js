342e947eec5e58991595ea881c095b9d4f0f3e6f
Allyson-SFelix <98405884+Allyson-SFelix@users.noreply.github.com>
2022-02-04 21:30:11 -0300

    Add files via upload

---
+++ Projetos/P-3/app.js
@@ -0,0 +1,42 @@
+var dados = document.querySelector("div.tudo h3 input");
+var resultado = document.querySelector("div.tudo div.resultado");
+var operacao = document.getElementById("operacao");
+const botao = document.getElementById("botao");
+
+console.log(botao);
+
+botao.addEventListener("click", () => {
+
+    resultado.innerHTML = ""
+
+    if (Number(dados.value) < 1 || Number(dados.value) > 10) {
+        resultado.innerHTML = "Digite um número de acordo com os"
+
+    } else {
+
+        var cv = dados.value;
+
+        for (i = 1; i <= 10; i++) {
+            switch (operacao.value) {
+                case "soma":
+                    var calculo = String(i) + " + " + dados.value + " = " + (Number(i) + Number(dados.value));
+                    break;
+                case "divisao":
+                    var calculo = divisor + " / " + dados.value + " = " + (Number(divisor) / dados.value);
+                    cv = (Number(cv) + Number(dados.value));
+                    break;
+                case "multiplicacao":
+                    var calculo = String(i) + " x " + dados.value + " = " + i * Number(dados.value);
+                    break;
+                case "subtracao":
+                    var calculo = String(i) + " - " + dados.value + " = " + (Number(i) - Number(dados.value));
+                    break;
+            }
+            console.log(calculo);
+            var cq = document.createElement("p");
+            cq.innerText = calculo;
+            resultado.appendChild(cq);
+
+        }
+    }
+})
